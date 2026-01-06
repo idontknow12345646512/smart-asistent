27ffec0bce6fc347ff01862117ab5cc0a3fd1515
idontknow12345646512 <159150410+idontknow12345646512@users.noreply.github.com>
2026-01-06 16:37:48 +0100

    Update shared.py

--- shared.py
+++ shared.py
@@ -1,12 +1,5 @@
-import streamlit as st
-
-# Tato funkce vytvoří jeden objekt v paměti serveru, který vidí úplně VŠICHNI
-@st.cache_resource
-def get_global_store():
-    return {
-        "logs": [],        # Tady budou zprávy všech lidí
-        "key_status": {},  # Tady bude stav 10 klíčů
-        "active_users": 0
-    }
-
-global_store = get_global_store()
+# shared.py
+global_store = {
+    "key_status": {},
+    "all_chats": {}  # Zde budeme ukládat chaty: {chat_id: {"title": str, "msgs": list}}
+}
